49fedc8581fcb0f0c59911f39b52e9bbf0ce7164
Carlos Eduardo Dev <kadu0410@gmail.com>
2022-06-10 19:15:43 -0300

    Update script.js

--- script.js
+++ script.js
@@ -10,28 +10,27 @@ window.addEventListener("load", () => {
         const secpass = () => {
             
             let min = Math.floor(sec/60);
-            let segundosRestantes = Math.floor(sec%60);
+            let segundosRestantes = sec % 60;
+
+
+            if (segundosRestantes < 10) {
+                segundosRestantes = "0" + segundosRestantes;
+            }
+
+            if (min <10 ) {
+                min = "0" + min;
+            }
+
+            countDiv.innerHTML = min + ":" + segundosRestantes;
+
+            if (sec >0) {
+                sec = sec - 1;
+            }
+            else {
+                countDiv.innerHTML = "Tempo Esgotado";
+            }
 
         }
-
-        if (segundosRestantes < 10) {
-            segundosRestantes = "0" + segundosRestantes;
-        }
-
-        if (min <10 ) {
-            min = "0" + min;
-        }
-
-        countDiv.innerHTML = min + ":" + segundosRestantes;
-
-        if (sec >0) {
-            sec = sec - 1;
-        }
-        else {
-            countDiv.innerHTML = "Acabou!";
-        }
-
-    
     
     const countDown = setInterval(() => secpass(), 1000);
 
